6a3343594732696f94629e2c9de2b950fb114ebd
Miah <revolution5117@gmail.com>
2020-09-11 09:22:03 -0700

    Problems running through array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
 
 const App = () => {
+  const [pdb,setpdb] = useState('')
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon-species/?offset=151&limit=20`)
+    .then((res) =>{
+      setpdb(res.data.results)
+      console.log(res)
+      
+    })
+  },[]
+  )
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +24,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character data = {pdb}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+const Character = props => {
+    const {data} = props
+    console.log(data)
+    return('')
+           
+            // data.map(eachpoke => {
+            //   return (
+            //    <div className = 'pokeName'>
+            //        <h1>{eachpoke.name}</h1>
+            //        <a href={eachpoke.url}>Data</a>
+            //    </div>
+            //   )
+            // })
+          
+          
+        
+   
+}
+
+export default Character
